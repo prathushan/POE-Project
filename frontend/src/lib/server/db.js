f673eb0a3828d2147883bcbfa5a35cb4c4fcbc86
prathushan <119046937+prathushan@users.noreply.github.com>
2026-03-23 15:31:13 +0530

    updated db config for railway

--- frontend/src/lib/server/db.js
+++ frontend/src/lib/server/db.js
@@ -1,8 +1,8 @@
 import mysql from 'mysql2/promise';
-
+ 
 export const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'poe'
+  host: process.env.MYSQLHOST || 'localhost',
+  user: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPASSWORD || '',
+  database: process.env.MYSQLDATABASE || 'poe'
 });
