dba14704c867feec78fd503c071a73003b3d500e
prathushan <119046937+prathushan@users.noreply.github.com>
2026-03-23 18:19:36 +0530

    Fix DB connection using env variables

--- frontend/src/lib/server/db.js
+++ frontend/src/lib/server/db.js
@@ -1,8 +1,19 @@
-import mysql from 'mysql2/promise';
+// import mysql from 'mysql2/promise';
  
+// export const db = mysql.createPool({
+//   host: process.env.MYSQLHOST || 'localhost',
+//   user: process.env.MYSQLUSER || 'root',
+//   password: process.env.MYSQLPASSWORD || '',
+//   database: process.env.MYSQLDATABASE || 'poe'
+// });
+
+import mysql from 'mysql2/promise';
+import { env } from '$env/dynamic/private';
+
 export const db = mysql.createPool({
-  host: process.env.MYSQLHOST || 'localhost',
-  user: process.env.MYSQLUSER || 'root',
-  password: process.env.MYSQLPASSWORD || '',
-  database: process.env.MYSQLDATABASE || 'poe'
+  host: env.DB_HOST,
+  port: env.DB_PORT,
+  user: env.DB_USER,
+  password: env.DB_PASSWORD,
+  database: env.DB_NAME,
 });
